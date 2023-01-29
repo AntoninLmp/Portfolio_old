2826610e87c832fff2294313a3330c089937d770
Antonin Lampin <antonin.lampin@efrei.net>
2023-01-29 18:17:35 +0100

    Update start page
    Add transition & black window

--- my_portfolio/src/components/StartName.js
+++ my_portfolio/src/components/StartName.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import { useState } from 'react';
 import './StartName.css';
 
-class StartName extends React.Component {
-    render() {
-        return (
-            <div className='start-div'>
-                <h1 className='nameStart animation_text'>Antonin Lampin</h1>
-                <h2 className='descriptionStart animation_text'>Software Engeenier</h2>
-            </div>
-        );
-    }
+
+function StartName() {
+    const [className, setclassName] = useState('start-div');
+
+    setTimeout(() => {
+        setclassName('start-div blackwindow')
+    }, 7000);
+
+    return (
+        <div className={className}>
+            <h1 className='nameStart animation_text'>Antonin Lampin</h1>
+            <h2 className='descriptionStart animation_text'>Software Engeenier</h2>
+        </div >
+    );
 }
 
 export default StartName
