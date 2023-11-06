3b6db07bb3ffcd3b498f9e2ae2a6d286257a151f
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2023-11-06 17:42:45 +0100

    Navbar button

--- my_portfolio/src/components/global/Navbar.js
+++ my_portfolio/src/components/global/Navbar.js
@@ -1,13 +1,23 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import './index.css';
 
 function Navbar() {
+    const [menuOpen, setMenuOpen] = useState(false); 
+    const [transitionMenu, setTransitionMenu] = useState(null)
+
+    
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        if (menuOpen) {setTransitionMenu('list_item')}
+      };
+
     return (
         <nav className='Navbar'>
             <div className="d_flex mx-1e">
                 <div class="container nav-container">
-                    <input class="checkbox" type="checkbox" name="" id="" />
+                    <input class="checkbox" type="checkbox" name="" id="" onClick={toggleMenu}/>
                     <div class="hamburger-lines">
                         <span class="line line1"></span>
                         <span class="line line2"></span>
@@ -16,24 +26,28 @@ function Navbar() {
                 </div>
                 <p className="text_menu">Menu</p>
             </div>
-            <nav >
-                <ul className="list_display">
-                    <li>
-                        <Link to={"/Home"} className="c_green">Home</Link>
-                    </li>
-                    <li>
-                        <Link to={"/AntoninLampin"} className="c_black">Moi</Link>
-                    </li>
-                    <li >
-                        <Link to={"/Study"} className="c_green">Etudes</Link>
-                    </li>
-                    <li>
-                        <Link to={"/Project"} className="c_black">Projet</Link>
-                    </li>
-                    <li >
-                        <Link to={"/parcoursProfessionnel"} className="c_green">Parcours pro.</Link>
-                    </li>
-                </ul>
+
+            {/* Afficher les liens si le menu est ouvert */}
+            <nav>
+                { menuOpen ? (
+                    <ul className="list_display">
+                        <li className={transitionMenu}>
+                            <Link to={"/Home"} className="c_green">Home</Link>
+                        </li>
+                        <li className={transitionMenu}>
+                            <Link to={"/AntoninLampin"} className="c_white">Moi</Link>
+                        </li>
+                        <li className={transitionMenu}>
+                            <Link to={"/Study"} className="c_green">Etudes</Link>
+                        </li>
+                        <li className={transitionMenu}>
+                            <Link to={"/Project"} className="c_white">Projet</Link>
+                        </li>
+                        <li className={transitionMenu}>
+                            <Link to={"/parcoursProfessionnel"} className="c_green">Parcours pro.</Link>
+                        </li>
+                    </ul>
+                ) : null }
             </nav>
         </nav>
     )
