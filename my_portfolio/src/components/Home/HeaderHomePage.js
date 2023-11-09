a48c2982611cb627a4b2d1f64c0ad356bcf358c7
Antonin Lmp <73228586+AntoninLmp@users.noreply.github.com>
2023-11-09 10:58:07 +0100

    Fix: Typewriter

--- my_portfolio/src/components/Home/HeaderHomePage.js
+++ my_portfolio/src/components/Home/HeaderHomePage.js
@@ -8,21 +8,14 @@ function HeaderHomePage() {
             <section className='font_raleway'>
                 <p className='Header_desc'>Bienvenue sur mon <span className='c_green bold oblique'>portfolio</span> !</p>
                 <div className='d_flex'>
-                    <p className='Header_desc d_flex'>A travers ce site, vous découvrirez que je suis             
+                    <p className='Header_desc d_flex'>A travers ce site, vous découvrirez que je suis</p>
+                    <p className='Header_desc d_flex'>
                         <Typewriter options={{
                                 strings: [' étudiant en alternance', ' développeur Logiciel junior', ' scouts et guides de France'],
                                 autoStart: true,
                                 loop: true,
                             }} /> 
                     </p>
-                    {/* <p className='Header_desc'>A travers ce site, vous découvrirez que je suis </p>
-                    <p className='Header_desc'>
-                        <Typewriter options={{
-                                strings: [' étudiant en alternance', ' développeur Logiciel junior', ' scouts et guides de France'],
-                                autoStart: true,
-                                loop: true,
-                            }} /> 
-                    </p> */}
                 </div>
                 {/* <p className='Header_desc'>Vous pourrez en apprendre plus sur mes projets, mes études où encore sur moi même !</p> */}
             </section>
